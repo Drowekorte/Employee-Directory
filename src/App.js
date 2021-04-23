1971aa3f38e30f25b864052341f456ad6ebc7411
Drowekorte <drowekorte@gmail.com>
2021-04-23 08:58:10 -0700

    Working on the search feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./Header";
+import SearchForm from "./SearchForm";
 import TableHeader from "./TableHeader";
 import EmployeeCard from "./EmployeeCard";
 import API from "./API";
 
 class App extends React.Component {
   state = {
+    search: "",
     employees: []
   };
 
@@ -19,35 +21,39 @@ class App extends React.Component {
     console.log("updated state employees: ", this.state.employees)
   };
 
+  filterEmployees = (employee) => {
+    if (employee.name.includes(this.state.search)) {
+      return true;
+    }
+    if (employee.email.includes(this.state.search)) {
+      return true;
+    }
+  }
 
   render() {
-    console.log(this.state.employees)
     return (
-
       <div className="container-lg">
-        <Header/>
-        <TableHeader/>
+        <Header />
+        <div>
+        <SearchForm
+          search={this.state.search}
+        />
+      </div>
+        <TableHeader />
         <div>
           {this.state.employees.map((burrito) => (
-              <EmployeeCard
-                key={Math.random()}
-                image={burrito.picture.medium}
-                name={burrito.name.first + " " + burrito.name.last}
-                phone={burrito.phone}
-                email={burrito.email}
-                
-                  
-                  />
-          )
-          )
-          }
-            </div>
-          </div>
-
-
-
+            <EmployeeCard
+              key={Math.random()}
+              image={burrito.picture.medium}
+              name={burrito.name.first + " " + burrito.name.last}
+              phone={burrito.phone}
+              email={burrito.email}
+            />
+          ))}
+        </div>
+      </div>
     )
   }
 }
 
-  export default App;
+export default App;
